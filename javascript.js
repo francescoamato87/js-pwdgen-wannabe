9a60a773c7eff1b8bb16a88488dd1d26a2a38783
FrancescoAmato87 <francesco.duff@gmail.com>
2020-10-13 16:03:20 +0200

    Start JS

---
+++ javascript.js
@@ -0,0 +1,15 @@
+alert("nomecognomecolorepreferito19");
+
+var nome = window.prompt ("Ciao, inserisci il tuo nome: ");
+
+document.getElementById("titolo").innerHTML = "Benvenuto " + nome;
+
+var cognome = window.prompt ("inserisci il tuo Cognome: ");
+
+document.getElementById("informazioni").innerHTML = "Cognome: " + cognome;
+
+var colore = window.prompt ("qual'é il tuo colore preferito?!? ");
+
+document.getElementById("colore").innerHTML = "Colore preferito: " + colore;
+
+// due linee sono i commenti!!!
