5ef1f91daa3f9f52fcdee837d44f406fb8e0940b
FrancescoAmato87 <francesco.duff@gmail.com>
2020-10-14 09:51:30 +0200

    Password

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-alert("nomecognomecolorepreferito19");
+alert("nome" + "cognome" + "colorepreferito19");
 
 var nome = window.prompt ("Ciao, inserisci il tuo nome: ");
 
@@ -11,5 +11,11 @@ document.getElementById("informazioni").innerHTML = "Cognome: " + cognome;
 var colore = window.prompt ("qual'é il tuo colore preferito?!? ");
 
 document.getElementById("colore").innerHTML = "Colore preferito: " + colore;
+
+// Costruzione password
+var password = nome + cognome + colore + '19';
+console.log(password);
+
+document.getElementById('password').innerHTML = 'La tua password è ' + password;
 
 // due linee sono i commenti!!!
